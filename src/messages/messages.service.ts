ac2bdbd6d306f1b03e25128fb382cfe7f872846f
Akram Ebadi <aki.ebadi@gmail.com>
2024-03-11 18:26:27 +0330

    create Nest app before refactor

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,22 @@
+import { MessagesRepository } from "./messages.repository"
+
+export class MessagesService {
+    messageRepo: MessagesRepository
+    constructor() {
+    // Service is creating its own dependencies
+    // DONT DO THIS ON REAL APPS
+    this.messageRepo = new MessagesRepository();// temporary code
+    }
+
+    findOne(id: string) {
+        return this.messageRepo.findOne(id);
+    }
+    
+    findAll() {
+        return this.messageRepo.findAll();
+    }
+
+    create(content: string) {
+        return this.messageRepo.create(content);
+    }
+}
